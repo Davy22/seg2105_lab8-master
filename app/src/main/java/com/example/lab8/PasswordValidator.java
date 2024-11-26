3c6ed96434588804918aee3f5977be77ef0f887e
Davy22 <shadavid019@gmail.com>
2024-11-26 15:07:12 -0500

    jjd

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -14,6 +14,8 @@ public class PasswordValidator {
 
             // If both conditions are met, the password is valid
             if (hasUpperCase && hasLowerCase) return true;
+
+
         }
         return false;
     }
